99525eb8c31b69a7974e92727acf6665acf03a2c
Marcelino de la Cruz <marcelino.delacruz@urjc.es>
2022-11-09 12:30:02 +0000

    version 0.5

--- R/LF.gof.R
+++ R/LF.gof.R
@@ -27,7 +27,12 @@ LF.gof<- function(X, rmin=NULL, rmax=NULL, na.rm=TRUE){
 
     # compute summary statistics for the observed (i.e., first) pattern 
     Hmeans <- rowSums (H[,-1],  na.rm = na.rm)/(s-1)
+    
     u <- sum((H[,1]-Hmeans)^2, na.rm=T)
+    
+    #width of the distance interval. the second value of the r vector (the first one is 0)
+    delta_tk <- X$r[2]
+
      
      # compute summary statistics for the simulated patterns
      for ( i in 2:s) {
@@ -36,5 +41,23 @@ LF.gof<- function(X, rmin=NULL, rmax=NULL, na.rm=TRUE){
        }
      p <-  1-((rank(u)[1]-1)/(s))
      
-return(list(u=u[1], p=p,na.count.by.r=na.count.by.r))
+return(list(u=u[1]*delta_tk, p=p,na.count.by.r=na.count.by.r))
+
+# computatiopn according to Baddeley et al. 2014  https://doi.org/10.1890/13-2042.1
+# dr <- X$r[2]
+# H0 <- X$obs
+# simfuns <- data.frame(attributes(X)$simfuns)[,-1]
+# m <- ncol(simfuns)
+# a <- ((m+1)/m)^2
+
+# H1bar <- apply(simfuns, 1, mean)
+# H2bar <- (m*H1bar /(m+1)) +(H0/(m+1))
+
+# U <- a * sum(dr*(H0-H2bar)^2)
+# for ( i in 1:m) {
+#           U<- c(U, a*sum(dr*(simfuns[,i]-H2bar)^2, na.rm=T)) 
+#       }
+#     p <-  1-((rank(U)[1]-1)/(m+1))
+# return( u = U[1], p = p)
+
 }
